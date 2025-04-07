b5727119d64cddffe737a6c55676d9f1f73edeb3
ganesh <gchimmula@gmail.com>
2025-04-07 13:32:14 -0400

    Fix and Style share pop-up

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ let shareButton = document.querySelector("#share-button");
 shareButton.addEventListener("click", function () {
     let shareOptions = document.querySelector(".article-footer__share-menu");
     let display = shareOptions.style.display;
+    let footerNode = document.querySelector(".article-card__footer");
     if (!display || display=="none"){
         shareOptions.style.display = "flex";
     }else{
