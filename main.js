f4e9a47185342ba69d97b0ec75dacd2ffe28d8b3
ganesh <gchimmula@gmail.com>
2025-04-07 13:43:16 -0400

    Update share popup class name

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 let shareButton = document.querySelector("#share-button");
 
 shareButton.addEventListener("click", function () {
-    let shareOptions = document.querySelector(".article-footer__share-menu");
+    let shareOptions = document.querySelector(".article-footer__share-menu--mobile");
     let display = shareOptions.style.display;
     let footerNode = document.querySelector(".article-card__footer");
     if (!display || display=="none"){
