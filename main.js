320711860772e6fadb309ef83a94541d1a0ef1c1
ganesh <gchimmula@gmail.com>
2025-04-07 22:09:23 -0400

    Add handlers to close popup when clicked outside and Enter

--- main.js
+++ main.js
@@ -1,13 +1,34 @@
 let shareButton = document.querySelector("#share-button");
 let shareOptions = document.querySelector(".article-card__share-options");
 
-shareButton.addEventListener("click", function () {
-    let display = shareOptions.style.display;
-    if (!display || display=="none"){
-        shareOptions.style.display = "flex";
-        shareButton.classList.add('article-card__share-button--active')
-    }else{
-        shareOptions.style.display = "none";
-        shareButton.classList.remove('article-card__share-button--active')
-    }
+// shareButton.addEventListener("click", function () {
+//     let display = shareOptions.style.display;
+//     if (!display || display=="none"){
+//         shareOptions.style.display = "flex";
+//         shareButton.classList.add('article-card__share-button--active')
+//     }else{
+//         shareOptions.style.display = "none";
+//         shareButton.classList.remove('article-card__share-button--active');
+//     }
+// })
+
+shareButton.addEventListener("click", ()=>{
+    shareButton.classList.toggle('article-card__share-button--active');
+    shareOptions.classList.toggle('article-card__share-options--active');
 })
+
+// // Closes share box when clicked outside
+// document.addEventListener('click', (e)=> {
+//     if(!shareButton.contains(e.target) && !shareOptions.contains(e.target)) {
+//         shareButton.classList.remove('article-card__share-button--active');
+//         shareOptions.classList.remove('article-card__share-options--active')
+//     }
+// })
+
+// // keyboard accesibility
+// shareButton.addEventListener("keydown", (e) => {
+//     if(e.key === "Enter" || e.key === ""){
+//         e.preventDefault();
+//         shareButton.click();
+//     }
+// })
